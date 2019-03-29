c3b4f8c0f32830392601db2a7295055fc2e76f62
mehrotsh <31403228+mehrotsh@users.noreply.github.com>
2019-03-29 16:08:56 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -3,8 +3,8 @@ import requests
 #url = 'https://gm:9443/api/v1/'
 url = 'https://vp/api/public/monitoredSystems'
 headers = {'Accept':'application/vnd.com.teradata.viewpoint-v1.0+json'}
-response = requests.get(url, verify=False,auth=('admin','teradata'), headers=headers)
-#esponse = requests.get(url, verify=False, auth=('viewpoint','teradata'))
+response = requests.get(url, verify=False,auth=('user','pass'), headers=headers)
+#esponse = requests.get(url, verify=False, auth=('user','pass'))
 print (response.json())
 print(response.status_code)
 
